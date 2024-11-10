f6f0b4cfc89d0c84d4c2ea4598c55539ce063db4
Gustavo Fonseca <gustavo.fonseca.214@my.csun.edu>
2024-11-09 16:11:24 -0800

    Commiting changes as new branch

---
+++ Assets/Scripts/Pieces.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pieces: MonoBehaviour
+{
+    public GameObject controller;
+    public GameObject movePlate;
+    private int xPos = -1;
+    private int yPos = -1;
+    private const float pieceSpacing = 0.5f;
+    private const float xOffSet = -2.5f;
+    private const float yOffSet = -2.5f;
+    private string player;
+    public Sprite B_King, B_Queen, B_Knight, B_Bishop, B_Rook, B_Pawn;
+    public Sprite W_King, W_Queen, W_Knight, W_Bishop, W_Rook, W_Pawn;
+    private Dictionary<string, Sprite> pieceSprites;
+
+    public void PieceSprites() {
+        pieceSprites = new Dictionary<string, Sprite> {
+            {"B_King", B_King}, {"B_Queen", B_Queen}, {"B_Bishop", B_Bishop}, {"B_Knight", B_Knight}, {"B_Rook", B_Rook}, {"B_Pawn",B_Pawn},
+            {"W_King", W_King}, {"W_Queen", W_Queen}, {"W_Bishop", W_Bishop}, {"W_Knight", W_Knight}, {"W_Rook", W_Rook}, {"W_Pawn",W_Pawn}
+        };
+    }
+
+    public void Activate() {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+        SetCoords();
+
+        if(pieceSprites.ContainsKey(this.name)) {
+            this.GetComponent<SpriteRenderer>().sprite = pieceSprites[this.name];
+        }
+    }
+
+    public void SetCoords() {
+        float x = xPos * pieceSpacing + xOffSet;
+        float y = yPos * pieceSpacing + yOffSet;
+        this.transform.position = new Vector3(x,y,-1.0f);
+    }
+
+    public int GetXPos() {
+        return xPos;
+    }
+
+    public int GetYPos() {
+        return yPos;
+    }
+
+    public void SetXPos(int x) {
+        xPos = x;
+    }
+
+    public void SetYPos(int y) {
+        yPos = y;
+    }
+}
