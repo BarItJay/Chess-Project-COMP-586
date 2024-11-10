060a5fbee7e177c716986d48459beef9ea54a862
Gustavo Fonseca <gustavo.fonseca.214@my.csun.edu>
2024-11-10 15:27:25 -0800

    Pieces generate in correct position

--- Assets/Scripts/Pieces.cs
+++ Assets/Scripts/Pieces.cs
@@ -8,9 +8,9 @@ public class Pieces: MonoBehaviour
     public GameObject movePlate;
     private int xPos = -1;
     private int yPos = -1;
-    private const float pieceSpacing = 0.5f;
-    private const float xOffSet = -2.5f;
-    private const float yOffSet = -2.5f;
+    private const float pieceSpacing = 0.96f;
+    private const float xOffSet = -3.4f;
+    private const float yOffSet = -3.5f;
     private string player;
     public Sprite B_King, B_Queen, B_Knight, B_Bishop, B_Rook, B_Pawn;
     public Sprite W_King, W_Queen, W_Knight, W_Bishop, W_Rook, W_Pawn;
