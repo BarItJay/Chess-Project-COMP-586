fdbcdecda96eb7edfbee576f7abf9c5e1e28f0bb
Gustavo Fonseca <gustavo.fonseca.214@my.csun.edu>
2024-12-14 22:52:13 -0800

    Initial UI Implementations and buttons for multiplayer

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GameUI : MonoBehaviour {
+    public static GameUI Instance {set; get;}
+
+    [SerializeField] private Animator menuAnimator;
+    
+    private void Awake() {
+        Instance = this;
+    }
+
+    //Buttons
+    public void OnLocalGameButton() {
+        menuAnimator.SetTrigger("NoMenu");
+    }
+
+    public void OnOnlineGameButton() {
+        menuAnimator.SetTrigger("OnlineMenu");
+    }
+
+    public void OnOnlineHostButton() {
+        menuAnimator.SetTrigger("HostMenu");
+    }
+
+    public void OnOnlineConnectButton() {
+        Debug.Log("OnOnlineConnectButton");
+    }
+
+    public void OnOnlineBackButton() {
+        menuAnimator.SetTrigger("StartMenu");
+    }
+
+    public void OnHostBackButton() {
+        menuAnimator.SetTrigger("OnlineMenu");
+    }
+}
